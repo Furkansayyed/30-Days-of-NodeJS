e1d3ae736d9b88d693edd2bc68eac32676dd8590
Furkan Sayyed <sayyedfurkan115@gmail.com>
2024-02-09 15:56:43 +0530

    day 9 completed

---
+++ Day-9/index.js
@@ -0,0 +1,13 @@
+// Which of the following are examples of node modules?
+
+// a. Express
+
+// b. Body-parser
+
+// c. Socket.io
+
+// d. All of the above
+
+// Answer ->
+// All of the options provided (a. Express, b. Body-parser, c. Socket.io) are examples of Node.js modules.
+
