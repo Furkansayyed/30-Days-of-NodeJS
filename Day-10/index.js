cd149a3c37d1b895f15b8015490bd2f0490024a1
Furkan Sayyed <sayyedfurkan115@gmail.com>
2024-02-10 11:10:53 +0530

    day 10 completed

---
+++ Day-10/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for accessing the root ("/") returns the "index.html" file from the "public" directory
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
